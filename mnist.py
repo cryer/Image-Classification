cb98064ec76f191cee6586242c83db015083da56
kurumi <935211204@qq.com>
2017-12-11 18:46:50 +0800

    Add files via upload

---
+++ mnist.py
@@ -0,0 +1,97 @@
+"""
+实现mnist分类 
+"""
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.utils.data as Data
+import torchvision
+import matplotlib.pyplot as plt
+
+torch.manual_seed(1)
+
+EPOCH = 1
+BATCH_SIZE = 50
+LR = 0.001
+
+train_data = torchvision.datasets.MNIST(
+    root='./mnist/',
+    train=True,
+    transform=torchvision.transforms.ToTensor(),                                                 
+    download=False,
+)
+train_loader = Data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
+
+
+test_data = torchvision.datasets.MNIST(root='./mnist/', train=False)
+
+test_x = Variable(torch.unsqueeze(test_data.test_data, dim=1), volatile=True).type(torch.FloatTensor)[:2000]/255.   
+test_y = test_data.test_labels[:2000]
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Sequential(        
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=16,
+                kernel_size=5,
+                stride=1,
+                padding=2,
+            ),                             
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),   
+        )
+        self.conv2 = nn.Sequential(        
+            nn.Conv2d(16, 32, 5, 1, 2),    
+            nn.ReLU(),
+            nn.MaxPool2d(2),               
+        )
+        self.out = nn.Linear(32 * 7 * 7, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = x.view(x.size(0), -1)       
+        output = self.out(x)
+        return output, x   
+
+
+cnn = CNN()
+
+optimizer = torch.optim.Adam(cnn.parameters(), lr=LR)
+loss_func = nn.CrossEntropyLoss()                      
+
+from matplotlib import cm
+from sklearn.manifold import TSNE
+def plot_with_labels(lowDWeights, labels):
+    plt.cla()
+    X, Y = lowDWeights[:, 0], lowDWeights[:, 1]
+    for x, y, s in zip(X, Y, labels):
+        c = cm.rainbow(int(255 * s / 9)); plt.text(x, y, s, backgroundcolor=c, fontsize=9)
+    plt.xlim(X.min(), X.max()); plt.ylim(Y.min(), Y.max()); plt.title('Visualize last layer'); plt.show(); plt.pause(0.01)
+
+plt.ion()
+for epoch in range(EPOCH):
+    for step, (x, y) in enumerate(train_loader):
+        b_x = Variable(x)
+        b_y = Variable(y)
+        output = cnn(b_x)[0]
+        loss = loss_func(output, b_y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+	  
+        if step % 50 == 0:
+            test_output, last_layer = cnn(test_x)
+            pred_y = torch.max(test_output, 1)[1].data.squeeze()
+            accuracy = sum(pred_y == test_y) / float(test_y.size(0))
+            print('Epoch: ', epoch, '| train loss: %.4f' % loss.data[0], '| test accuracy: %.2f' % accuracy)
+            #TSNE可视化
+	      tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
+            plot_only = 500
+            low_dim_embs = tsne.fit_transform(last_layer.data.numpy()[:plot_only, :])# (batch_size, 32 * 7 * 7)
+            labels = test_y.numpy()[:plot_only]
+            plot_with_labels(low_dim_embs, labels)
+plt.ioff()
+
